b3130b4ffafdd5a53b0b8fa3be6028af7fe94a4e
Kibika <steshykibika@gmail.com>
2022-08-07 13:28:29 -0700

    file storage

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,7 @@ Base Model for AirBnB clone
 
 import uuid
 from datetime import datetime
+import models
 
 format_dt = "%Y-%m-%dT%H:%M:%S.%f"
 
@@ -24,6 +25,7 @@ class BaseModel():
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """str representation"""
@@ -33,6 +35,8 @@ class BaseModel():
     def save(self):
         """Updates time given change"""
         self.updated_at = datetime.now()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """Create dictionary representation of the instance
---
+++ models/engine/file_storage.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Serializes instances to a JSON file and deserializes JSON file to instances
+"""
+
+import json
+from datetime import datetime
+from models import *
+
+
+class FileStorage():
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """
+	returns the dictionary __objects
+        """
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """Set in __objects obj with key <obj_class_name>.id"""
+        class_name = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(class_name, obj.id)] = obj
+
+    def save(self):
+        """ file storage
+        """
+        dict_to_json = {}
+        for key, value in FileStorage.__objects.items():
+            dict_to_json[key] = value.to_dict()
+        with open(FileStorage.__file_path, "w", encoding='utf-8') as f:
+            json.dump(dict_to_json, f)
+
+    def reload(self):
+        """Deserialize the JSON file __file_path to __objects, if it exists."""
+        try:
+            with open(FileStorage.__file_path) as f:
+                objdict = json.load(f)
+                for o in objdict.values():
+                    cls_name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(cls_name)(**o))
+        except:
+            pass
